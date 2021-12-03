a6467ef8c52b926264f8cd5e44291783264b364c
KuraiOni <softwaremahmoud1@gmail.com>
2021-12-04 01:44:31 +0200

    Started the java project

---
+++ src/Main.java
@@ -0,0 +1,176 @@
+import java.util.Scanner;	 
+import javax.swing.JOptionPane;
+import java.util.Random;
+
+
+public class Main {
+
+	public static void main(String[] arges) {
+		
+		
+		
+//		System.out.println("First Sentence");
+//		System.out.println("Some Other Line\n");
+//		System.out.println("Hello\n");
+//		
+//		
+//		// This a comment
+//		
+//		/*
+//		 * This is 
+//		 * a comment
+//		 * with multiple
+//		 * lines
+//		 */
+//		
+//		
+//		//Variables
+//		
+//		/*
+//		 * list of datatype:
+//		 * byte
+//		 * short
+//		 * float
+//		 * long
+//		 */
+//		
+//		int x = 23;
+//		double y = 8.63765;
+//		boolean value = true;
+//		char wtvr = '@';
+//		String sent = "This is a string";
+//		
+//		System.out.println(x);
+//		
+//		
+//		// Swaping Variables
+//		
+//		
+//		String X = "Water";
+//		String Y = "Orange";
+//		String temp;
+//		
+//		temp = X; 
+//		X = Y;
+//		Y = temp;
+//		
+//		System.out.println("X: "+X);
+//		System.out.println("Y: "+Y);
+//		
+//		// Scanner 
+//		
+//		Scanner scanner = new Scanner(System.in);
+//		
+//		System.out.println("What is your name? ");
+//		String name = scanner.nextLine();
+//		System.out.println("How old are you? ");
+//		int age = scanner.nextInt();
+//		scanner.nextLine();
+//		System.out.println("What is your favorite food? ");
+//		String food = scanner.nextLine();
+//		
+//		System.out.println("Your name is "+name);
+//		System.out.println("You are "+age+" years old");
+//		System.out.println("Your favorite food is "+food);
+		
+		
+		//GUI Interface
+		
+//		String id = JOptionPane.showInputDialog("Enter your name");
+//		JOptionPane.showMessageDialog(null, "Hello "+id);
+//		int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+//		JOptionPane.showMessageDialog(null, "You are "+age+" years old");
+//		double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height	"));
+//		JOptionPane.showMessageDialog(null, "You are"+height+" cm tall");
+		
+		
+		
+		// Math Class
+		
+//		double X;
+//		double Y;
+//		double Z;
+//		
+//		
+//		Scanner scanner = new Scanner(System.in);
+//		
+//		System.out.println("Enter the value of X: ");
+//		X = scanner.nextDouble();
+//		System.out.println("Enter the value of Y: ");
+//		Y = scanner.nextDouble();
+//		
+//		
+//		Z = Math.sqrt((X*X)+(Y*Y));
+//		
+//		System.out.println("The hypotrnus is: "+Z);
+//		
+//		scanner.close();
+		
+		//Random Class
+		
+//		Random random = new Random();
+//		
+//		int X = random.nextInt(10);
+//		System.out.println(X);
+//		
+//		double Y = random.nextDouble();
+//		System.out.println(Y);
+		
+		
+		// if Statements
+		
+//		Scanner scanner = new Scanner(System.in);
+//		
+//		System.out.println("What is your name? ");
+//		String X = scanner.nextLine();
+//		System.out.println("How old are you? ");
+//		double Y = scanner.nextDouble();
+//		scanner.nextLine();
+//		System.out.println("How tall are you? ");
+//		double Z = scanner.nextDouble();
+//		scanner.nextLine();
+//		
+//		
+//		System.out.println("Your name is: "+X);
+//		System.out.println("You are "+Y+" years old");
+//		System.out.println("You are "+Z+" cm tall");
+//		
+//		scanner.close();
+//		
+//		
+//		if(Y>=18 && Z>=175) {
+//			System.out.println("You are to be in the back");
+//		}
+//		else if(Y>=13 && Z>=165) {
+//			System.out.println("You are to be in the middle");
+//		}
+//		else {
+//			System.out.println("You are to be in the front");
+//		}
+		
+		
+		// switch = statement that allows a variable to be tested for equality against a list of values
+		  
+//		  String day = "Friday";
+//		  
+//		  switch(day) {
+//		   case "Sunday": System.out.println("It is Sunday!");
+//		   break;
+//		   case "Monday": System.out.println("It is Monday!");
+//		   break;
+//		   case "Tuesday": System.out.println("It is Tuesday!");
+//		   break;
+//		   case "Wednesday": System.out.println("It is Wednesday!");
+//		   break;
+//		   case "Thursday": System.out.println("It is Thursday!");
+//		   break;
+//		   case "Friday": System.out.println("It is Friday!");
+//		   break;
+//		   case "Saturday": System.out.println("It is Saturday!");
+//		   break;
+//		   default: System.out.println("That is not a day!");
+//		  }
+		
+}
+
+}
