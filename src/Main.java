a37b055a81a3a36d9c216c83aea505bcb8ac1b35
KuraiOni <softwaremahmoud1@gmail.com>
2021-12-04 03:15:32 +0200

    Continued Coding

--- src/Main.java
+++ src/Main.java
@@ -171,6 +171,118 @@ public class Main {
 //		   default: System.out.println("That is not a day!");
 //		  }
 		
+		
+		// While Loops
+		
+//		Scanner scanner = new Scanner(System.in);
+//		String name = "";
+//		
+//		
+//		while(name.isBlank()) {
+//			System.out.println("Enter your name: ");
+//			name = scanner.nextLine();
+//		}
+//		
+//		System.out.println("Your name is: "+name);
+//		
+//		do{
+//			System.out.println("Enter your name: ");
+//			name = scanner.nextLine();
+//		}while(name.isBlank()) ;
+//		
+//		System.out.println("Your name is: "+name);
+		
+		
+		// For loops 
+		
+		
+//		for(int i=0; i<=10; i++ ) {
+//			System.out.println(i);
+//		}
+//		
+//		System.out.println("Hello");
+//		
+//		for(int i=10; i>=0; i--) {
+//			System.out.println(i);
+//		}
+		
+		//Nested Loopes
+		
+//		Scanner scanner = new Scanner(System.in);
+//		int rows;
+//		int columns;
+//		String symbol = "";
+//		
+//		
+//		System.out.println("Enter the number of rows: ");
+//		rows = scanner.nextInt();
+//		System.out.println("Enter the number of columns: ");
+//		columns = scanner.nextInt();
+//		System.out.println("Enter the symbol: ");
+//		symbol = scanner.next();
+//		
+//		for(int i=1; i<=rows; i++) {
+//			System.out.println();
+//			for(int j=1; j<=columns; j++) {
+//				System.out.print(symbol);
+//			}
+//		}
+		
+		
+		//Arrays
+		
+//		String[] cars = new String[3];
+//		
+//		cars[0] = "tesla";
+//		cars[1] = "mustang";
+//		cars[2] = "camaro";
+//		
+//		for(int i=0; i<cars.length; i++) {
+//			System.out.println(cars[i]);
+//		}
+		
+		// 2D array = an array of arrays
+		
+//				String[][] cars = 	{	
+//										{"Camaro","Corvette","Silverado"},
+//										{"Mustang","Ranger","F-150"},
+//										{"Ferrari","Lambo","Tesla"}
+//									};
+//				
+//				/*
+//				cars[0][0] = "Camaro";
+//				cars[0][1] = "Corvette";
+//				cars[0][2] = "Silverado";
+//				cars[1][0] = "Mustang";
+//				cars[1][1] = "Ranger";
+//				cars[1][2] = "F-150";
+//				cars[2][0] = "Ferrari";
+//				cars[2][1] = "Lambo";
+//				cars[2][2] = "Tesla";
+//				*/
+//				
+//				for(int i=0; i<cars.length; i++) {
+//					System.out.println();
+//					for(int j=0; j<cars[i].length; j++) {
+//						System.out.print(cars[i][j]+" ");
+//					}
+//				}
+		
+		 //   reference data types have access to useful methods
+		  
+//		  String name = "Bro";
+		  
+			//boolean result = name.equalsIgnoreCase("bro");
+		  //int result = name.length();
+		  //char result = name.charAt(0);
+		  //int result = name.indexOf("o");
+		  //boolean result = name.isEmpty();
+		  //String result = name.toUpperCase();
+		  //String result = name.toLowerCase();
+		  //String result = name.trim();
+		  //String result = name.replace('o', 'a');
+		   
+		  //System.out.println(result);
 }
 
 }
